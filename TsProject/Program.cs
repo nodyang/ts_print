f5f7c1f688d7d7c71a861dc7a61f9bdc356cde8b
nodyang <yangw@chims.cn>
2021-03-24 09:36:43 +0800

    first

---
+++ TsProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using Exceptionless;
+using NewLife.Log;
+
+namespace TsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
